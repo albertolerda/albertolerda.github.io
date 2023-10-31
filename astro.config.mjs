47d5ccb56e01152ca9570056f9902b6a45e5fb9a
Alberto Lerda <alberto@dyne.org>
2023-10-31 00:15:22 +0000

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     markdown: {
-        shikiConfig: {
-          theme: "monokai",
-        },
+      shikiConfig: {
+        theme: "monokai",
       },
+    },
+    site: 'https://albertolerda.github.io',
 });
